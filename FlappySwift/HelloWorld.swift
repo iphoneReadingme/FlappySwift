c4a2ff2225ba805ce5a2f683192825e36c53b982
iphoneReadingme <readingme@126.com>
2014-06-05 18:29:18 +0800

    测试w代码

---
+++ FlappySwift/HelloWorld.swift
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.swift
+//  FlappySwift
+//
+//  Created by Giordano Scalzo on 02/06/2014.
+//  Copyright (c) 2014 Effective Code. All rights reserved.
+//
+
+import UIKit
+
+import SpriteKit
+
+class HelloWorld
+{
+	var labelNode: SKLabelNode!
+	var contentCreated: Bool
+	
+	init ()
+	{
+		contentCreated = false;
+	}
+	
+	func newHelloLabelNode() -> SKLabelNode
+	{
+		labelNode = SKLabelNode(text: "test");
+		labelNode.text = "Hello, World!"
+		labelNode.position =  CGPointMake(100, 0)
+		labelNode.fontSize = 40
+		labelNode.fontColor = UIColor.blueColor();
+		labelNode.color = UIColor.grayColor();
+		
+		labelNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(70, 60))
+//		labelNode.physicsBody.dynamic = false;
+		labelNode.physicsBody.affectedByGravity = false
+		
+		return labelNode;
+	}
+	
+	func newHelloSpriteNode() -> SKSpriteNode
+	{
+		var rock = SKSpriteNode();
+		rock.position = CGPointMake(50, 50);
+		rock.name = "rock";
+		rock.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(70, 60));
+		return rock;
+	}
+	
+	func update(pt:CGPoint)
+	{
+		labelNode.position = pt;
+	}
+}
+
+
+
